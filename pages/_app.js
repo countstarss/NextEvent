7f878c0172116c7e450c752656c4956b9b78ee62
lukeking <countstarss@163.com>
2024-07-20 19:29:20 +0800

    优化Next应用- 4. 在_app.js中添加通用头部信息

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 // pages/_app.js
 import '../styles/globals.css'; // 这里导入全局 CSS
 import Layout from '@/components/layout/layout';
+import Head from 'next/head';
 
 
 function MyApp({ Component, pageProps }) {
-    return <Layout><Component {...pageProps} /></Layout>
+    return <Layout>
+            <Head>
+                <title>Next Events</title>
+                <meta name="description" content="NextJS Events"/>
+                <meta name="viewport" content="initial-scale=1.0,width=device-width"/>
+            </Head>
+            <Component {...pageProps} />
+        </Layout>
     // return <Component {...pageProps} />
 }
 
